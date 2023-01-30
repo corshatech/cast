6362066045e4b85b51b379af2640055a95e3eb3c
ericatcorsha <eric@corsha.com>
2023-01-30 13:49:25 -0500

    Implementation of the findings framework (#8)
    Implements the backend Findings framework
    
    ---------
    Signed-off-by: ArcticLight <max@corsha.com>
    Co-authored-by: GitHub Action <action@github.com>

---
+++ ui/src/lib/analysis/reused-authentication.ts
@@ -0,0 +1,159 @@
+/* Copyright 2023 Corsha.
+   Licensed under the Apache License, Version 2.0 (the 'License');
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an 'AS IS' BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+import { Analysis, Finding, ReusedAuthentication } from "lib/findings";
+import conn from "../../lib/db";
+
+const query = `
+select
+  reused.auth,
+  srcCount.count,
+  authTimespan.min_timestamp,
+  authTimespan.max_timestamp,
+  sampleRequest.src->>'ip' as src_ip,
+  sampleRequest.src->>'port' as src_port,
+  sampleRequest.dst->>'ip' as dst_ip,
+  sampleRequest.dst->>'port' as dst_port,
+  sampleRequest.URI,
+  sampleRequest.timestamp
+FROM
+
+-- find all auth headers that have multiple sources
+(
+  SELECT
+    data->'request'->'headers'->>'Authorization' as auth
+  FROM traffic
+  WHERE
+    data->'request'->'headers'->>'Authorization' is not null
+  GROUP BY
+    auth
+  HAVING
+    count(distinct data->'src') > 1
+) as reused,
+
+-- find min and max timestamps for each reused auth header
+LATERAL (
+SELECT
+    min(t.occurred_at) as min_timestamp,
+    max(t.occurred_at) as max_timestamp
+FROM traffic as t
+WHERE reused.auth = t.data->'request'->'headers'->>'Authorization'
+GROUP BY reused.auth
+) as authTimespan,
+
+-- find the request counts for each source that reused the auth header
+LATERAL (
+  SELECT
+    t.data->'src' as src,
+    reused.auth as auth,
+    count(*) as count
+  FROM traffic AS t
+  WHERE reused.auth = t.data->'request'->'headers'->>'Authorization'
+  GROUP BY t.data->'src', reused.auth
+) as srcCount,
+
+-- find the most recent request made by each source that reused the auth header
+LATERAL (
+  SELECT
+   t.data->'src' as src,
+   t.data->'dst' as dst,
+   t.data->'request'->'absoluteURI' as URI,
+   t.occurred_at as timestamp
+   FROM traffic t
+   WHERE
+     srcCount.src = t.data->'src'
+   AND
+     srcCount.auth = t.data->'request'->'headers'->>'Authorization'
+   ORDER BY t.occurred_at DESC
+   LIMIT 1
+) as sampleRequest
+`;
+
+interface Row {
+  auth: string;
+  count: number;
+  min_timestamp: Date;
+  max_timestamp: Date;
+  src_ip: string;
+  src_port: string;
+  dst_ip: string;
+  dst_port: string;
+  URI: string;
+  timestamp: Date;
+}
+
+/** group the rows by their auth header */
+function groupByAuthHeader(rows: Row[]): Record<string, Row[]> {
+  const groups: Record<string, Row[]> = {};
+
+  rows.forEach((row: Row) => {
+    groups[row.auth] = [...(groups[row.auth] ?? []), row];
+  });
+
+  return groups;
+}
+
+function rowsToFinding(detectedAt: string, auth: string, rows: Row[]): Finding {
+  const occurredAt = {
+    start: rows[0].min_timestamp.toISOString(),
+    end: rows[0].max_timestamp.toISOString(),
+  };
+  const inRequests: ReusedAuthentication["data"]["inRequests"] = rows.map(x => ({
+    srcIp: x.src_ip,
+    srcPort: x.src_port,
+    proto: "tcp",
+    destIp: x.dst_ip,
+    destPort: x.dst_port,
+    URI: x.URI,
+    at: x.timestamp.toISOString(),
+    count: x.count,
+  }));
+
+  return {
+    type: "reused-auth",
+    name: "Reused Authentication",
+    severity: "medium",
+    occurredAt,
+    detectedAt,
+    data: {
+      auth,
+      inRequests,
+    },
+  };
+}
+
+export type QueryFunction = () => Promise<Row[]>;
+
+/** runnerPure is the AnalysisFunction free of external dependencies
+ *
+ * This function allows for dependency injection during unit testing
+ */
+export async function runnerPure(query: QueryFunction): Promise<Analysis> {
+  const rows = await query();
+  const reportedAt = new Date().toISOString();
+  const findings = Object.entries(groupByAuthHeader(rows)).map(([id, rows]) =>
+    rowsToFinding(reportedAt, id, rows),
+  );
+
+  return {
+    id: "reused-auth",
+    title: "Reused Authentication",
+    description: "An analysis that finds if multiple clients are using the same Authorization HTTP header value. Clients who use the same authorization header could be evidence of stolen credentials.",
+    severity: "medium",
+    reportedAt,
+    findings,
+  };
+}
+
+export async function runner(): Promise<Analysis> {
+  const queryFunction = async () => (await conn.query(query, [])).rows;
+  return runnerPure(queryFunction);
+}
---
+++ ui/src/lib/findings.ts
@@ -0,0 +1,133 @@
+/* Copyright 2023 Corsha.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+/** A human-readable string which accepts Markdown */
+export type MDString = string;
+
+/** Defined to be a string containing a date in UTC-aligned RFC-3339 format */
+export type DateString = string;
+
+export type OccurredAt = {
+  start: DateString;
+  end: DateString;
+} | {
+  at: DateString;
+};
+
+/** The set of severity levels */
+export const Severity = ["none", "low", "medium", "high", "critical"] as const;
+
+/** The severity of a finding. */
+export type Severity = typeof Severity[number];
+
+export interface Analysis {
+  /** Human-friendly machine-readable ID for analysis kind; e.g. CAST-C-103 */
+  id: string;
+  /** Human-readable title for analysis kind */
+  title: string;
+  /** Human-readable description for analysis. */
+  description: MDString;
+  /** Time that this analysis completed */
+  reportedAt: DateString;
+  /** URL for authoritative source covering weakness; i.e. CVE or CWE or OWASP link */
+  weaknessLink?: string;
+  /** Human-readable title of weakness ID/link; e.g. if it"s a CWE,
+   * this is something like "CWE-439" */
+  weaknessTitle?: string;
+  /** Severity of failed analysis; must not be "lower" than the
+   * severity of any of the individual findings contained within */
+  severity: Severity;
+  /** Findings reported by this analysis, if any. Omitted if empty. */
+  findings?: Finding[];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type IFinding<Type extends string, DataType extends Record<string, any>> = {
+  /** Internal type ID; belongs to the set of finding kinds */
+  type: Type;
+  /** Human-readable "name" of finding type */
+  name: string;
+  /**
+   * When the infraction occurred
+   *
+   * An RFC3339-format timestamp or range. Optional. If present, represents
+   * the time that an infraction occurred, if available and if
+   * different from DetectedAt. For example, when examining API logs,
+   * OccurredAt may indicate the timestamp from the logs while
+   * DetectedAt indicates the time that CAST processed the
+   * incident.
+   */
+  occurredAt?: OccurredAt;
+  /**
+   * The time when this Finding was generated
+   * An RFC3339-format timestamp of when this Finding was generated internally by CAST
+   */
+  detectedAt: DateString;
+
+  severity: Severity;
+  /** Contextual data describing the particular finding. Particular
+   * contents varies by finding kind. */
+  data: DataType;
+};
+
+export interface RequestContext {
+  srcIp: string;
+  srcPort: string;
+  proto: "tcp" | "udp" | "unknown";
+  destIp: string;
+  destPort: string;
+  URI?: string;
+  at: DateString;
+}
+
+export type ReusedAuthentication = IFinding<"reused-auth", {
+  /** The identifier for the particular reused authentication */
+  auth: string;
+  /** Minimum 2 RequestContext[], at least one unique record per srcIp
+   * involved in the auth sharing. */
+  inRequests: (RequestContext & { count: number })[];
+}>;
+
+export type ExpiredJWT = IFinding<"expired-jwt", {
+  /** The JWT of concern */
+  jwt: string;
+  /** DateString formatted timestamp of when the JWT expired */
+  expiredAt: DateString;
+  inRequest: RequestContext;
+}>;
+
+export type PasswordInURL = IFinding<"pass-in-url", {
+  /** The name of the query parameter suspected of containing a password. */
+  queryParam: string;
+  /**
+   * The request that triggered this finding. In-particular,
+   * the URI of this request should have a best-effort scrub to not contain
+   * the credential in the finding report; only report the suspicion of it
+   * containing a credential.
+   */
+  inRequest: RequestContext;
+}>;
+
+export type UseOfBasicAuth = IFinding<"use-of-basic-auth", {
+  inRequest: RequestContext;
+}>;
+
+export type Finding = ReusedAuthentication
+  | ExpiredJWT
+  | PasswordInURL
+  | UseOfBasicAuth/** A function that produces an analysis result */
+export type AnalysisFunction = () => Promise<Analysis>;
+
+/** Evaluates all analysis functions */
+export async function runAllAnalyses(analyses: AnalysisFunction[]): Promise<Analysis[]> {
+  const promises = analyses.map(f => f());
+  return Promise.all(promises);
+}

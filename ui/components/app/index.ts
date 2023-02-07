3ef0428547cc1a6792c5377c3eb45de3b035e1a1
Jason Pierce <jason@corsha.com>
2023-02-07 12:27:11 -0500

    feat: remove ref to FeatureComponent

--- ui/components/app/index.ts
+++ ui/components/app/index.ts
@@ -1,5 +1,4 @@
 export * from './Header/Header';
 export * from './Layout/Layout';
-export * from './FeatureComponent/FeatureComponent';
 export * from './page-sections/index';
 export * from './Summary';

a4d7b1ffb3ef103e614f5be2768f829645db2e85
Jason Pierce <jason@corsha.com>
2023-03-02 13:01:43 -0500

    feat: make typography section more readable

--- ui/components/app/page-sections/TypographySection/index.tsx
+++ ui/components/app/page-sections/TypographySection/index.tsx
@@ -24,19 +24,19 @@ export const TypographySection = () => {
         </Typography>
         <div className="grid grid-cols-4 justify-between items-start pt-12">
           <Typography variant="body1">
-            <span className="font-black">Body 1 Black</span>
+            <span className="font-black">Body 1 Black</span><br />
             <span className="font-black">Nunito Sans Black 16px</span>
           </Typography>
           <Typography variant="body1">
-            <span className="font-bold">Body 1 Bold</span>
+            <span className="font-bold">Body 1 Bold</span><br />
             <span className="font-bold">Nunito Sans Bold 16px</span>
           </Typography>
           <Typography variant="body1">
-            <span>Body 1 Regular</span>
+            <span>Body 1 Regular</span><br />
             <span>Nunito Sans Regular 16px</span>
           </Typography>
           <Typography variant="body1">
-            <span className="underline font-medium">Body 1 Underlined</span>
+            <span className="underline font-medium">Body 1 Underlined</span><br />
             <span className="underline font-medium">
               Nunito Sans Medium Underline 16px
             </span>
@@ -44,21 +44,21 @@ export const TypographySection = () => {
         </div>
         <div className="grid grid-cols-4 justify-between items-start pt-12">
           <Typography variant="body2">
-            <span className="font-bold">Body 2 Bold</span>
+            <span className="font-bold">Body 2 Bold</span><br />
             <span className="font-bold">Nunito Sans Bold 14px</span>
           </Typography>
           <Typography variant="body2">
-            <span>Body 2 Regular</span>
+            <span>Body 2 Regular</span><br />
             <span>Nunito Sans Regular 14px</span>
           </Typography>
           <Typography variant="body2">
-            <span className="underline font-medium">Body 2 Underlined</span>
+            <span className="underline font-medium">Body 2 Underlined</span><br />
             <span className="underline font-medium">
               Nunito Sans Medium Underline 14px
             </span>
           </Typography>
           <Typography variant="body2">
-            <span className="font-medium">All Caps</span>
+            <span className="font-medium">All Caps</span><br />
             <span className="font-black capitalize">
               Nunito Sans Black All Caps 14px
             </span>
@@ -66,19 +66,19 @@ export const TypographySection = () => {
         </div>
         <div className="grid grid-cols-4 justify-between items-start pt-12">
           <Typography variant="body3">
-            <span className="font-black">Body 3 Black</span>
+            <span className="font-black">Body 3 Black</span><br />
             <span className="font-black">Nunito Sans Black 12px</span>
           </Typography>
           <Typography variant="body3">
-            <span className="font-bold">Body 3 Bold</span>
+            <span className="font-bold">Body 3 Bold</span><br />
             <span className="font-bold">Nunito Sans Bold 12px</span>
           </Typography>
           <Typography variant="body3">
-            <span>Body 3 Regular</span>
+            <span>Body 3 Regular</span><br />
             <span>Nunito Sans Regular 12px</span>
           </Typography>
           <Typography variant="body3">
-            <span className="underline font-medium">Body 3 Underlined</span>
+            <span className="underline font-medium">Body 3 Underlined</span><br />
             <span className="underline font-medium">
               Nunito Sans Medium Underline 12px
             </span>
@@ -86,19 +86,19 @@ export const TypographySection = () => {
         </div>
         <div className="grid grid-cols-4 justify-between items-start pt-12">
           <Typography variant="body4">
-            <span className="font-black">Body 4 Black</span>
+            <span className="font-black">Body 4 Black</span><br />
             <span className="font-black">Nunito Sans Black 10px</span>
           </Typography>
           <Typography variant="body4">
-            <span className="font-bold">Body 4 Bold</span>
+            <span className="font-bold">Body 4 Bold</span><br />
             <span className="font-bold">Nunito Sans Bold 10px</span>
           </Typography>
           <Typography variant="body4">
-            <span>Body 4 Regular</span>
+            <span>Body 4 Regular</span><br />
             <span>Nunito Sans Regular 10px</span>
           </Typography>
           <Typography variant="body4">
-            <span className="underline font-medium">Body 4 Underlined</span>
+            <span className="underline font-medium">Body 4 Underlined</span><br />
             <span className="underline font-medium">
               Nunito Sans Medium Underline 10px
             </span>

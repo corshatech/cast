a9381e13b5d06140d35fd3201c3d4b284dbb2e7f
Eric Moritz <eric@corsha.com>
2023-03-13 14:27:20 -0400

    Check for kubeshark version, wait for curl pods to be ready

--- scripts/generate-pipeline-data.sh
+++ scripts/generate-pipeline-data.sh
@@ -15,10 +15,25 @@
 
 # configure the local environment to work with the skaffold
 
+if ! [ -x "$(command -v kubeshark)" ]; then
+    echo >&2 "ERROR: kubeshark is not installed, visit https://kubeshark.co/ to install"
+    exit 1
+fi
+
+KUBESHARK_VERSION="$(kubeshark version 2>&1)"
+if [ "$KUBESHARK_VERSION" != "38.5" ]; then
+    kubeshark version
+    echo "ERROR: incorrect version of kubeshark installed, please install 38.5"
+    exit 1
+fi
+
 svc=$1
 echo "service endpoint: $svc"
 
 export CONTEXT="docker-desktop"
+
+# clean up any existing kubeshark install
+kubectl --context="${CONTEXT}" delete ns kubeshark --wait=true || true
 
 # kubeshark tap
 nohup kubeshark --set kube.context="${CONTEXT}" tap -n cast "(httpbin*)" --set headless=true > kubeshark.out 2> kubeshark.err < /dev/null &
@@ -26,13 +41,31 @@ nohup kubeshark --set kube.context="${CONTEXT}" tap -n cast "(httpbin*)" --set h
 
 # Waiting for collector pod successfully connect to postgres and kubeshark
 collector=$(kubectl --context="${CONTEXT}" get pods --namespace=cast | grep cast-collector | cut -d' ' -f1)
-while ! kubectl --context="${CONTEXT}" logs -n cast ${collector}|grep -q "Starting export of records." ;do echo "Waiting for collector to be ready.\n";sleep 10;done
+while ! kubectl --context="${CONTEXT}" logs -n cast "${collector}"|grep -q "Starting export of records." ; do
+    echo "Waiting for collector to be ready."
+    sleep 10;
+done
 
-# create curl pod
+# create curl pods
+kubectl --context="${CONTEXT}" delete ns curl --wait=true || true
 kubectl --context="${CONTEXT}" create namespace curl
-kubectl --context="${CONTEXT}" -n curl delete pod curl
 kubectl --context="${CONTEXT}" run curl -n curl --image=curlimages/curl -- sleep 3600
 kubectl --context="${CONTEXT}" wait --for=condition=Ready pod/curl -n curl
+
+kubectl --context="${CONTEXT}" delete ns curl2 --wait=true || true
+kubectl --context="${CONTEXT}" create namespace curl2
+kubectl --context="${CONTEXT}" run curl -n curl2 --image=curlimages/curl -- sleep 3600
+kubectl --context="${CONTEXT}" wait --for=condition=Ready pod/curl -n curl2
+
+# wait until we can execute a command
+until kubectl --context="${CONTEXT}" exec -n curl curl -i -- true; do
+    echo "waiting for the curl container to become available"
+done
+
+until kubectl --context="${CONTEXT}" exec -n curl2 curl -i -- true; do
+    echo "waiting for the curl2 container to become available"
+done
+
 
 # password in url
 echo -e "\ninserting pass-in-url data\n"
@@ -42,7 +75,7 @@ arr=(
     "auth?auth=xyzzy"
     "pass2x?pass=xyzzy&pass=xyzzy2"
     "pass1x1?pass=xyzzy&password=xyzzy2"
-    )
+)
 
 for i in "${arr[@]}"
 do
@@ -56,7 +89,7 @@ jwts=(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjEyNjU3NTA0Njd9.yf73iGwy_ztPAZiKSc-qgtvBFNGQdKfRrOF7vrCp4j8"
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIsInRlc3QiOiJ0ZXN0In0.XtSo2lOAEjh1BnGejrCUY3y3F2A7p7ByFGA_4m3Iq-s"
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDJ9.UqRu8fGnUAmn-Z_wwsgGVNTXANkIiDdEbj-BdZRafks"
-    )
+)
 
 for i in "${jwts[@]}"
 do
@@ -73,10 +106,6 @@ echo -e "\ninserting reused-auth data\n"
 kubectl --context="${CONTEXT}" exec -n curl curl -i -- curl -s -w "\n" -H "Authorization: Bearer dummy-token1" "${svc}/headers?q=1"
 kubectl --context="${CONTEXT}" exec -n curl curl -i -- curl -s -w "\n" -H "Authorization: Bearer dummy-token2" "${svc}/headers?q=1"
 
-kubectl --context="${CONTEXT}" create namespace curl2
-kubectl --context="${CONTEXT}" -n curl2 delete pod curl
-kubectl --context="${CONTEXT}" run curl -n curl2 --image=curlimages/curl -- sleep 3600
-kubectl --context="${CONTEXT}" wait --for=condition=Ready pod/curl -n curl2
 
 kubectl --context="${CONTEXT}" exec -n curl2 curl -i -- curl -s -w "\n" -H "Authorization: Bearer dummy-token1" "${svc}/headers?q=1"
 kubectl --context="${CONTEXT}" exec -n curl2 curl -i -- curl -s -w "\n" -H "Authorization: Bearer dummy-token2" "${svc}/headers?q=1"

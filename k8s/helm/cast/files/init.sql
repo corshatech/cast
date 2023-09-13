4311926d3627423ac8152fa7ec42ae050c9d4e5b
Kaitlin Hipkin <katie@corsha.com>
2023-09-13 17:08:31 -0400

    include src ip from X-Real-IP header

--- k8s/helm/cast/files/init.sql
+++ k8s/helm/cast/files/init.sql
@@ -77,5 +77,12 @@ CREATE VIEW matview_traffic_ips AS
         'src' AS direction,
         TRIM(UNNEST(STRING_TO_ARRAY(data->'request'->'headers'->>'X-Forwarded-For', ','))) AS ip_addr
     FROM traffic
-    WHERE data->'request'->'headers'->>'X-Forwarded-For' IS NOT NULL);
+    WHERE data->'request'->'headers'->>'X-Forwarded-For' IS NOT NULL)
+    UNION
+    (SELECT
+         id AS traffic_id,
+         'src' AS direction,
+         data->'request'->'headers'->>'X-Real-IP' AS ip_addr
+     FROM traffic
+     WHERE data->'request'->'headers'->>'X-Real-IP' IS NOT NULL);
 SELECT * FROM matview_traffic_ips;

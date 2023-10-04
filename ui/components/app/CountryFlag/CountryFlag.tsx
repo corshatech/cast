816e9fd6974bae1a79e3b115da983a90c2e12bb6
Justin Thoms <justin@corsha.com>
2023-10-04 16:33:06 -0400

    Fixed styles for flags

--- ui/components/app/CountryFlag/CountryFlag.tsx
+++ ui/components/app/CountryFlag/CountryFlag.tsx
@@ -9,14 +9,15 @@ type Props = {
 export const CountryFlag: React.FC<Props> = ({isoCode, size = 30}) => {
     if (!(isoCode in Flags)) {
         return (
-            <Image 
-                src='./pirate.svg' 
-                alt={'Unknown flag icon'} 
-                width={size} 
-                height={size}
-            />
+            <div style={{width: `${size}px`, height: `${size*2/3}px`, position: 'relative'}}>
+                <Image 
+                    src='./pirate.svg' 
+                    alt={'Unknown flag icon'} 
+                    fill
+                />
+            </div>
         )
     }
     const Flag = Flags[isoCode as keyof typeof Flags];
-    return <Flag style={{width: `${size}px`}}/>
+    return <Flag style={{width: `${size}px`, display: 'inline'}}/>
 }

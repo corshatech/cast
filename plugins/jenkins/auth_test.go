c0f159f30ce135271bf54af63a41766749a970c3
Kaitlin Hipkin <katie@corsha.com>
2023-11-03 16:33:20 -0400

    test prepeareAuth prepares proper type of auth strategy for basic, cookie based, and unauthenticated

--- plugins/jenkins/auth_test.go
+++ plugins/jenkins/auth_test.go
@@ -78,6 +78,38 @@ func TestValidBasicCredentials(t *testing.T) {
 	}
 }
 
+func TestPrepareAuthWithValidBasicCredentialsReturnsBasicAuthStrategy(t *testing.T) {
+	testUsername := "user123"
+	testPassword := "pass456"
+
+	if err := os.Setenv(usernameEnv, testUsername); err != nil {
+		t.Fatalf("Failed to set env var %q to %q: %v", usernameEnv, testUsername, err)
+	}
+	defer os.Clearenv() // ensure no test state is left behind
+
+	if err := os.Setenv(passwordEnv, testPassword); err != nil {
+		t.Fatalf("Failed to set env var %q to %q: %v", passwordEnv, testPassword, err)
+	}
+
+	strat, err := prepareAuth()
+	if err != nil {
+		t.Fatalf("Failed to prepare authentication strategy: %v", err)
+	}
+
+	basicStrat, ok := strat.(*basicAuth)
+	if !ok {
+		t.Fatalf("Wanted basic auth strategy, but got something else: %+v", strat)
+	}
+
+	if got, want := basicStrat.username, testUsername; got != want {
+		t.Errorf("Wanted username %q, but got %q", want, got)
+	}
+
+	if got, want := basicStrat.password, testPassword; got != want {
+		t.Errorf("Wanted password %q, but got %q", want, got)
+	}
+}
+
 func TestUnsetSessionIDCookie(t *testing.T) {
 	name, value, err := sessionIDCookie()
 	if err != nil {
@@ -115,5 +147,46 @@ func TestValidSessionIDCookie(t *testing.T) {
 
 	if got, want := value, testCookieValue; got != want {
 		t.Errorf("Wanted cookie value %q, but got %q", want, got)
+	}
+}
+
+func TestPrepareAuthWithValidSessionIDCookieReturnsSessionCookieAuthStrategy(t *testing.T) {
+	testCookieName := "mySessionID"
+	testCookieValue := "very-secret-session-id"
+
+	testCookie := fmt.Sprintf("%s=%s", testCookieName, testCookieValue)
+
+	if err := os.Setenv(sessionIDEnv, testCookie); err != nil {
+		t.Fatalf("Failed to set env var %q to %q: %v", sessionIDEnv, testCookie, err)
+	}
+	defer os.Clearenv() // ensure no test state is left behind
+
+	strat, err := prepareAuth()
+	if err != nil {
+		t.Fatalf("Failed to prepare authentication strategy: %v", err)
+	}
+
+	cookieStrat, ok := strat.(*sessionCookieAuth)
+	if !ok {
+		t.Fatalf("Wanted session cookie auth strategy, but got something else: %+v", strat)
+	}
+
+	if got, want := cookieStrat.cookieName, testCookieName; got != want {
+		t.Errorf("Wanted cookie name %q, but got %q", want, got)
+	}
+
+	if got, want := cookieStrat.cookieValue, testCookieValue; got != want {
+		t.Errorf("Wanted cookie value %q, but got %q", want, got)
+	}
+}
+
+func TestPrepareAuthWithMissingEnvValuesReturnsNilAuthStrategy(t *testing.T) {
+	strat, err := prepareAuth()
+	if err != nil {
+		t.Fatalf("Failed to prepare authentication strategy: %v", err)
+	}
+
+	if strat != nil {
+		t.Errorf("Wanted nil auth strategy when no env variables are set, but got non-nil: %+v", strat)
 	}
 }

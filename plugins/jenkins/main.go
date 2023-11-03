7b80209506c9ddef0411d84d0421b74e66d7a901
Kaitlin Hipkin <katie@corsha.com>
2023-11-03 12:39:51 -0400

    pull req url building into helper func, only fatally fail in main()

--- plugins/jenkins/main.go
+++ plugins/jenkins/main.go
@@ -18,11 +18,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -43,21 +44,10 @@ const (
 )
 
 func main() {
-	tld := os.Getenv(tldEnv)
-	if tld == "" {
-		log.Fatalf("Required environment variable %s is not set!", tldEnv)
-	}
-
-	// See this article for more details on the tree parameter:
-	// https://www.cloudbees.com/blog/taming-jenkins-json-api-depth-and-tree
-	requestBaseURL, err := url.JoinPath(tld, "/asynchPeople/api/json")
+	requestURL, err := buildRequestURL()
 	if err != nil {
-		log.WithError(err).Fatal("Failed to build request URL")
+		log.WithError(err).Fatal("Failed to prepare URL for HTTP request")
 	}
-
-	requestURL := fmt.Sprintf("%s?%s", requestBaseURL, "tree=users[lastChange,project[fullName,url],user[id,fullName,absoluteURL,property[_class,address]{,15}]]")
-
-	log.WithField("requestURL", requestURL).Info("Successfully built URL to fetch Jenkins user data")
 
 	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
 	defer cancel()
@@ -96,4 +86,28 @@ func main() {
 
 	log.WithField("resultsLength", len(data.Users)).Info("Scan completed. Skipping writing results to CAST DB...")
 	log.Info("Done.")
+}
+
+func buildRequestURL() (string, error) {
+	tld := os.Getenv(tldEnv)
+	if tld == "" {
+		msg := fmt.Sprintf("Required environment variable %s is not set!", tldEnv)
+		log.Error(msg)
+		return "", errors.New(msg)
+	}
+
+	// See this article for more details on the tree parameter:
+	// https://www.cloudbees.com/blog/taming-jenkins-json-api-depth-and-tree
+	requestBaseURL, err := url.JoinPath(tld, "/asynchPeople/api/json")
+	if err != nil {
+		msg := fmt.Sprintf("Failed to build request URL: %v", err)
+		log.WithError(err).Fatal(msg)
+		return "", errors.New(msg)
+	}
+
+	requestURL := fmt.Sprintf("%s?%s", requestBaseURL, "tree=users[lastChange,project[fullName,url],user[id,fullName,absoluteURL,property[_class,address]{,15}]]")
+
+	log.WithField("requestURL", requestURL).Info("Built URL for querying Jenkins user data")
+
+	return requestURL, nil
 }
